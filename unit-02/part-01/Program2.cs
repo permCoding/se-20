66eb17edba38c649c0a8a10cc3cd9ca5fb12ed57
Андрей Беляков <ttxiom@gmail.com>
2020-05-30 13:14:16 +0500

    Create Program2.cs

---
+++ unit-02/part-01/Program2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //int a = new Int32();
+        System.Int32 a = -2; // int 00000010 - 11111110
+        Object ob = new Object();
+        ob = (byte)a; // 0 .. 255 - 00000000 - 11111111
+        Console.WriteLine(ob.ToString());
+        // теоретический вопрос про дополнит код числа и
+        // про явное приведение к другому типу данных
+        byte c = 254;
+        Console.WriteLine(ob.Equals(c));
+        ;
+    }
+}
